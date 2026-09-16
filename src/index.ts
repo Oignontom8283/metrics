34e18865334d5a4b8def68af79f039d562219e62
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-09-16 23:09:45 +0200

    Add KV namespace

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+interface Env {
+  METRICS_KV: KVNamespace;
+}
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
